6988bc832969f0585664cd414b0e4a1f1fe4f86f
Francisco Rafart <francisco.rafart@gmail.com>
2018-03-14 21:57:33 -0400

    third commit

--- euler23.js
+++ euler23.js
@@ -3,26 +3,24 @@
 function properDivisors(x){
   let res = []
   let half = Math.floor(x/2)
-  console.log(half)
   for (let i = half; i>0; i--){
     if (x%i == 0)
       res.push(i)
   }
   return res
 }
-function isPerfect(x, array){
+
+//function abundance 1: abundance, -1 deficient, 0: perfect
+function isAbundant(x, array){
   let sum=0;
   for (let a of array){
     sum+=a
   }
+
   if (sum == x)
-    return true
-
-  return false
-
+    return 0;
+  else if (sum > x)
+    return 1;
+  else
+    return -1;
 }
-
-console.log(isPerfect(28, properDivisors(28)));
-//function isAbundantnumber
-
-//function isDeficient
